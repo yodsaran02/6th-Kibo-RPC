4e819073e99f3338a7bb097e6f868cb118a95de9
Yodsaran Panomratanarak <70568348+yodsaran02@users.noreply.github.com>
2025-05-27 15:15:01 +0700

    upload files

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -0,0 +1,75 @@
+package jp.jaxa.iss.kibo.rpc.sampleapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+import org.opencv.core.Mat;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+        // The mission starts.
+        api.startMission();
+
+        // Move to a point.
+        Point point = new Point(10.9d, -9.92284d, 5.195d);
+        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+
+        // Get a camera image.
+        Mat image = api.getMatNavCam();
+
+        /* ******************************************************************************** */
+        /* Write your code to recognize the type and number of landmark items in each area! */
+        /* If there is a treasure item, remember it.                                        */
+        /* ******************************************************************************** */
+
+        // When you recognize landmark items, let’s set the type and number.
+        api.setAreaInfo(1, "item_name", 1);
+
+        /* **************************************************** */
+        /* Let's move to each area and recognize the items. */
+        /* **************************************************** */
+
+        // When you move to the front of the astronaut, report the rounding completion.
+        point = new Point(11.143d, -6.7607d, 4.9654d);
+        quaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+        api.reportRoundingCompletion();
+
+        /* ********************************************************** */
+        /* Write your code to recognize which target item the astronaut has. */
+        /* ********************************************************** */
+
+        // Let's notify the astronaut when you recognize it.
+        api.notifyRecognitionItem();
+
+        /* ******************************************************************************************************* */
+        /* Write your code to move Astrobee to the location of the target item (what the astronaut is looking for) */
+        /* ******************************************************************************************************* */
+
+        // Take a snapshot of the target item.
+        api.takeTargetItemSnapshot();
+    }
+
+    @Override
+    protected void runPlan2(){
+       // write your plan 2 here.
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write your plan 3 here.
+    }
+
+    // You can add your method.
+    private String yourMethod(){
+        return "your method";
+    }
+}
